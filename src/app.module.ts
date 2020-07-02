d0f0b24c7da4b7e7745cf6e7920ca547eae68009
Francisco <fgoinhas@companieshouse.gov.uk>
2020-07-02 16:29:17 +0100

    refactor: moved all controllers to suppressions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,8 @@
 import { Module } from '@nestjs/common';
-import { LandingController } from './suppressions/landing/landing.controller';
-import { AddressDetailsController } from './suppressions/address-details/address-details.controller';
-import { ApplicantDetailsController } from './suppressions/applicant-details/applicant-details.controller';
-import { DocumentDetailsController } from './suppressions/document-details/document-details.controller';
-import { ServiceAddressController } from './suppressions/service-address/service-address.controller';
-import { PaymentController } from './suppressions/payment/payment.controller';
-import { PaymentReferenceService } from './suppressions/payment/payment-reference/payment-reference.service';
 import { CommonModule } from './common/common.module';
+import { SuppressionsModule } from './suppressions/suppressions.module';
 
 @Module({
-  imports: [CommonModule],
-  controllers: [
-    LandingController,
-    AddressDetailsController,
-    ApplicantDetailsController,
-    DocumentDetailsController,
-    ServiceAddressController,
-    PaymentController
-  ],
-  providers: [PaymentReferenceService]
+  imports: [SuppressionsModule]
 })
 export class AppModule { }
---
+++ src/suppressions/suppressions.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { LandingController } from './landing/landing.controller';
+import { AddressDetailsController } from './address-details/address-details.controller';
+import { ApplicantDetailsController } from './applicant-details/applicant-details.controller';
+import { DocumentDetailsController } from './document-details/document-details.controller';
+import { ServiceAddressController } from './service-address/service-address.controller';
+import { PaymentController } from './payment/payment.controller';
+import { PaymentReferenceService } from './payment/payment-reference/payment-reference.service';
+import { CommonModule } from 'src/common/common.module';
+
+@Module({
+    imports: [CommonModule],
+    controllers: [
+        LandingController,
+        AddressDetailsController,
+        ApplicantDetailsController,
+        DocumentDetailsController,
+        ServiceAddressController,
+        PaymentController
+    ],
+    providers: [PaymentReferenceService]
+})
+export class SuppressionsModule { }

8e7a0b66784a508f5940194cb078d513ee3ddc59
Francisco <fgoinhas@companieshouse.gov.uk>
2020-07-02 16:24:48 +0100

    refactor: changed landing page uri

--- src/common/routes/routes.constants.ts
+++ src/common/routes/routes.constants.ts
@@ -1,5 +1,5 @@
 export const BASE_URI = `/suppress-my-details`;
-export const LANDING_PAGE_URI = `${BASE_URI}/landing`;
+export const LANDING_PAGE_URI = `${BASE_URI}`;
 export const APPLICANT_DETAILS_URI = `${BASE_URI}/applicant-details`;
 export const ADDRESS_DETAILS_URI = `${BASE_URI}/address-details`;
 export const DOCUMENT_DETAILS_URI = `${BASE_URI}/document-details`;
--- src/common/services/template-constant-provider/template-constant-provider.service.ts
+++ src/common/services/template-constant-provider/template-constant-provider.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { BASE_URI } from 'src/common/routes/routes.constants';
 
 @Injectable()
 export class TemplateConstantProviderService {
     public getServiceConstants(): any {
         return {
-            serviceName: 'Apply to remove your home address from the Companies House register (SR01)'
+            serviceName: 'Apply to remove your home address from the Companies House register (SR01)',
+            baseURI: BASE_URI
         }
     }
 }

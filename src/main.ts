b0b856f35075ac8a83a11562312fd4da4367842d
Francisco <fgoinhas@companieshouse.gov.uk>
2020-07-03 09:14:32 +0100

    feat: added basic .env file

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import * as nunjucks from 'nunjucks';
 import { join } from 'path';
 import * as express from 'express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -14,14 +15,16 @@ async function bootstrap() {
   app.use('/public', express.static(join(__dirname, '..', 'public')));
   app.use('/', express.static(join(__dirname, '..', 'node_modules/govuk-frontend/')));
 
+  const env: string = app.get(ConfigService).get('NODE_ENV');
+
   nunjucks.configure(['views', 'node_modules/govuk-frontend'], {
     autoescape: true,
     express: app,
     web: {
       async: true,
-      useCache: false,
+      useCache: env === 'dev' ? false : true,
     },
-    watch: true,
+    watch: env === 'dev' ? true : false,
   });
 
   await app.listen(4000);
